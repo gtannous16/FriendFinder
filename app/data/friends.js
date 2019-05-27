e3969b459035adde7e963ed64d987339d7a8de50
Gigi Tannous <gtannous16@gmail.com>
2019-05-26 20:15:44 -0400

    added scores for 5 friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,96 +3,96 @@ var FriendsList = [
         "name": "Monica Geller",
         "photo":"https://1.bp.blogspot.com/-vnsJ5BiF4-M/WNsT_z6wD6I/AAAAAAAAOXo/qstUVqqmBB8iUhMF8L82V_3KFAfhyDc6wCLcB/s320/6.png",
         "scores":[
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            ""
+            "3",
+            "1",
+            "5",
+            "1",
+            "2",
+            "3",
+            "5",
+            "1",
+            "2",
+            "4"
         ]
     },
     {
         "name": "Ross Geller",
         "photo":"https://4.bp.blogspot.com/-dZ6Rn17bva0/WNsT_Zr9MSI/AAAAAAAAOXU/LwiZPJ5PowQ-Z5tmo1RW5rVYFehBxGpoQCLcB/s320/3.png",
         "scores":[
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            ""
+            "2",
+            "5",
+            "2",
+            "1",
+            "4",
+            "1",
+            "3",
+            "1",
+            "5",
+            "5"
         ]
     },
     {
         "name": "Rachel Green",
         "photo":"https://1.bp.blogspot.com/-cjX-M_LZeFU/WNsT_TEAJOI/AAAAAAAAOXc/5P_syqjudxQXuSAPSHtWnlxSa4t-W-MmgCLcB/s320/1.png",
         "scores":[
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            ""
+            "5",
+            "1",
+            "3",
+            "1",
+            "1",
+            "2",
+            "1",
+            "5",
+            "3",
+            "5"
         ]
     },
     {
         "name": "Joey Tribbiani",
         "photo":"https://4.bp.blogspot.com/-mnsUOK2mhXg/WNsT_whRdLI/AAAAAAAAOXk/qx5tKr5ofHgqkLcVwsbpEgQyVvlFOq7yQCLcB/s320/5.png",
         "scores":[
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            ""
+            "1",
+            "1",
+            "5",
+            "1",
+            "1",
+            "1",
+            "1",
+            "5",
+            "5",
+            "3"
         ]
     },
     {
         "name": "Chandler Bing",
         "photo":"https://4.bp.blogspot.com/-NrDriR-OK88/WNsT_WXXfwI/AAAAAAAAOXY/ddSB9FDVoNsFt65NxXrTeQuz6ujPR6IvgCLcB/s320/2.png",
         "scores":[
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            ""
+            "1",
+            "2",
+            "3",
+            "5",
+            "5",
+            "1",
+            "1",
+            "2",
+            "5",
+            "3"
         ]
     },
     {
         "name": "Phoebe Buffay",
         "photo":"https://1.bp.blogspot.com/-D1uuTO7ZEIM/WNsT_hC2nBI/AAAAAAAAOXg/7dvWZ6gkz0YfcesQVEITRw5rcnwzJ6CxgCLcB/s320/4.png",
         "scores":[
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            ""
+            "1",
+            "1",
+            "1",
+            "5",
+            "5",
+            "1",
+            "1",
+            "5",
+            "4",
+            "1"
         ]
     }
 ]
